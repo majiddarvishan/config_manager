81d90a3c109eaf4ebe5ecdf9f25177b7468bb09e
Majid Darvishan <majiddarvishan@outlook.com>
2025-12-02 13:34:32 +0330

    Fix response

--- http_server.go
+++ http_server.go
@@ -6,8 +6,6 @@ import (
 	"encoding/json"
 	"fmt"
 	"io"
-
-	// "net"
 	"net/http"
 	"os"
 
@@ -17,52 +15,38 @@ import (
 
 type http_server struct {
 	address string
-	port    int16
+	port    int
 	api_key string
 
 	manager *Manager
 }
 
 func NewHttpServer(m *Manager, conf *Node) *http_server {
-	hs := &http_server{
-		manager: m,
+	hs := &http_server{manager: m}
+
+	addrNode, err := conf.At("address")
+	if err == nil {
+		hs.address, _ = addrNode.GetString()
 	}
 
-	p, err := conf.At("address")
-	if err != nil {
-		fmt.Printf("Error %v is occured when reading address", err)
-	}
-	hs.address, err = p.GetString()
-	if err != nil {
-		fmt.Printf("Error %v is occured when reading address", err)
+	portNode, err := conf.At("port")
+	if err == nil {
+		if v, _ := portNode.GetInt(); v > 0 {
+			hs.port = v
+		}
 	}
 
-	p, err = conf.At("port")
-	if err != nil {
-		fmt.Printf("Error %v is occured when reading port", err)
-	}
-	val, err := p.GetInt()
-	if err != nil {
-		fmt.Printf("Error %v is occured when reading port", err)
-	}
-	hs.port = int16(val)
-
-	p, err = conf.At("api_key")
-	if err != nil {
-		fmt.Printf("Error %v is occured when reading api_key", err)
-	}
-	hs.api_key, err = p.GetString()
-	if err != nil {
-		fmt.Printf("Error %v is occured when reading api_key", err)
+	keyNode, err := conf.At("api_key")
+	if err == nil {
+		hs.api_key, _ = keyNode.GetString()
 	}
 
 	return hs
 }
 
 func (hs *http_server) Start() {
-	sm := http.NewServeMux()
-
-	sm.HandleFunc("/config", hs.handleConfig)
+	mux := http.NewServeMux()
+	mux.HandleFunc("/config", hs.handleConfig)
 
 	addr := fmt.Sprintf("%s:%d", hs.address, hs.port)
 
@@ -71,159 +55,122 @@ func (hs *http_server) Start() {
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowedHeaders:   []string{"Content-Type", "Authorization", "X-API-Key"},
 		AllowCredentials: true,
-	}).Handler(sm)
+	}).Handler(mux)
 
-	err := http.ListenAndServe(addr, handler)
-	if err != nil {
+	if err := http.ListenAndServe(addr, handler); err != nil {
 		fmt.Printf("error on listening server: %s\n", err)
 		os.Exit(1)
 	}
 }
 
 func (hs *http_server) handleConfig(w http.ResponseWriter, r *http.Request) {
-	if r.Method == "GET" {
-		hs.on_get_request(w, r)
-	} else if r.Method == "POST" {
-		hs.on_post_request(w, r)
-	} else if r.Method == "OPTIONS" {
-		hs.on_options_request(w, r)
+	switch r.Method {
+	case http.MethodGet:
+		hs.onGet(w, r)
+	case http.MethodPost:
+		hs.onPost(w, r)
+	case http.MethodOptions:
+		hs.onOptions(w)
+	default:
+		writeError(w, http.StatusMethodNotAllowed, "method not allowed")
 	}
 }
 
-func (hs *http_server) on_get_request(w http.ResponseWriter, r *http.Request) {
+func (hs *http_server) onGet(w http.ResponseWriter, r *http.Request) {
 	if !hs.check_access(r) {
-		hs.unauthorized_access_resp(w)
+		writeError(w, http.StatusUnauthorized, "unauthorized")
 		return
 	}
 
-	hs.latest_config_state(w)
+	hs.latestConfigState(w)
 }
 
-func (hs *http_server) on_post_request(w http.ResponseWriter, r *http.Request) {
+func (hs *http_server) onPost(w http.ResponseWriter, r *http.Request) {
 	if !hs.check_access(r) {
-		hs.unauthorized_access_resp(w)
+		writeError(w, http.StatusUnauthorized, "unauthorized")
 		return
 	}
+
+	defer r.Body.Close()
 
 	body, err := io.ReadAll(r.Body)
 	if err != nil {
-		w.Header().Set("access_control_allow_origin", "*")
-		w.Header().Set("content_type", "text/html")
-		w.WriteHeader(http.StatusBadRequest)
-		io.WriteString(w, fmt.Sprintf("could not read body: %s", err))
+		writeError(w, http.StatusBadRequest, fmt.Sprintf("could not read body: %s", err))
 		return
 	}
 
-	var body_json = orderedmap.New()
-	if err = json.Unmarshal([]byte(body), &body_json); err != nil {
-		w.Header().Set("access_control_allow_origin", "*")
-		w.Header().Set("content_type", "text/html")
-		w.WriteHeader(http.StatusBadRequest)
-		io.WriteString(w, fmt.Sprintf("could not read body: %s", err))
+	var bodyJSON = orderedmap.New()
+	if err := json.Unmarshal(body, &bodyJSON); err != nil {
+		writeError(w, http.StatusBadRequest, fmt.Sprintf("could not parse JSON: %s", err))
 		return
 	}
 
-	val, present := body_json.Get("op")
-	if !present {
-		w.Header().Set("access_control_allow_origin", "*")
-		w.Header().Set("content_type", "text/html")
-		w.WriteHeader(http.StatusBadRequest)
-		io.WriteString(w, fmt.Sprintf("op tag is not present"))
-		return
-	}
-	var op = val.(string)
-
-	val, present = body_json.Get("path")
-	if !present {
-		w.Header().Set("access_control_allow_origin", "*")
-		w.Header().Set("content_type", "text/html")
-		w.WriteHeader(http.StatusBadRequest)
-		io.WriteString(w, fmt.Sprintf("path tag is not present"))
-		return
-	}
-	var path = val.(string)
-
-	val, present = body_json.Get("value")
-	if !present {
-		w.Header().Set("access_control_allow_origin", "*")
-		w.Header().Set("content_type", "text/html")
-		w.WriteHeader(http.StatusBadRequest)
-		io.WriteString(w, fmt.Sprintf("value tag is not present"))
-		return
-	}
-	// var value = val.(orderedmap.OrderedMap)
-	var value = val
-
-	val, present = body_json.Get("config_hash")
-	if !present {
-		w.Header().Set("access_control_allow_origin", "*")
-		w.Header().Set("content_type", "text/html")
-		w.WriteHeader(http.StatusBadRequest)
-		io.WriteString(w, fmt.Sprintf("config_hash tag is not present"))
-		return
-	}
-	var config_hash = val.(string)
-
-	// var config_hash = body_json["config_hash"].(string)
-	if config_hash != HashSHA256(*(hs.manager.source.getConfig())) {
-		fmt.Println("Config hash is invalid, application config is modified from else where")
+	op, err := getString(bodyJSON, "op")
+	if err != nil {
+		writeError(w, http.StatusBadRequest, err.Error())
 		return
 	}
 
-	if op == "insert" {
-		val, present = body_json.Get("index")
-		if !present {
-			w.Header().Set("access_control_allow_origin", "*")
-			w.Header().Set("content_type", "text/html")
-			w.WriteHeader(http.StatusBadRequest)
-			io.WriteString(w, fmt.Sprintf("index tag is not present"))
+	path, err := getString(bodyJSON, "path")
+	if err != nil {
+		writeError(w, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	value, _ := bodyJSON.Get("value")
+
+	configHash, err := getString(bodyJSON, "config_hash")
+	if err != nil {
+		writeError(w, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	currentHash := HashSHA256(*(hs.manager.source.getConfig()))
+	if configHash != currentHash {
+		writeError(w, http.StatusConflict, "config hash mismatch: config was modified elsewhere")
+		return
+	}
+
+	switch op {
+	case "insert":
+		idx, err := getIndex(bodyJSON)
+		if err != nil {
+			writeError(w, http.StatusBadRequest, err.Error())
 			return
 		}
-		var index = int(val.(float64))
-
-		err := hs.manager.insert(path, index, value)
-        if err != nil {
-            w.Header().Set("access_control_allow_origin", "*")
-			w.Header().Set("content_type", "text/html")
-			w.WriteHeader(http.StatusBadRequest)
-			io.WriteString(w, err.Error())
-			return
-        }
-	} else if op == "remove" {
-		val, present = body_json.Get("index")
-		if !present {
-			w.Header().Set("access_control_allow_origin", "*")
-			w.Header().Set("content_type", "text/html")
-			w.WriteHeader(http.StatusBadRequest)
-			io.WriteString(w, fmt.Sprintf("index tag is not present"))
+		if err := hs.manager.insert(path, idx, value); err != nil {
+			writeError(w, http.StatusBadRequest, err.Error())
 			return
 		}
-		var index = int(val.(float64))
 
-		hs.manager.remove(path, index)
-	} else if op == "replace" {
+	case "remove":
+		idx, err := getIndex(bodyJSON)
+		if err != nil {
+			writeError(w, http.StatusBadRequest, err.Error())
+			return
+		}
+		hs.manager.remove(path, idx)
+
+	case "replace":
 		hs.manager.replace(path, value)
-	} else {
-		w.Header().Set("access_control_allow_origin", "*")
-		w.Header().Set("content_type", "text/html")
-		w.WriteHeader(http.StatusBadRequest)
-		io.WriteString(w, "unsupported operation")
+
+	default:
+		writeError(w, http.StatusBadRequest, "unsupported operation")
 		return
 	}
 
-	hs.latest_config_state(w)
+	hs.latestConfigState(w)
 }
 
-func (hs *http_server) on_options_request(w http.ResponseWriter, r *http.Request) {
-	//Allow CORS here By * or specific origin
+func (hs *http_server) onOptions(w http.ResponseWriter) {
 	w.Header().Set("Access-Control-Allow-Origin", "*")
 	w.Header().Set("Access-Control-Allow-Headers", "Origin, Content-Type, X-API-Key")
-	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS")
-	// w.WriteHeader(http.StatusOK)
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+	writeSuccess(w, orderedmap.New()) // returns success: true
 }
 
 func (hs *http_server) check_access(r *http.Request) bool {
-	return (r.Header.Get("X-API-Key") == hs.api_key)
+	return r.Header.Get("X-API-Key") == hs.api_key
 }
 
 func HashSHA256(s string) string {
@@ -231,41 +178,84 @@ func HashSHA256(s string) string {
 	return hex.EncodeToString(sum[:])
 }
 
-func (hs *http_server) latest_config_state(w http.ResponseWriter) {
-	var conf_json = orderedmap.New()
-	json.Unmarshal([]byte(*(hs.manager.source.getConfig())), &conf_json)
+func (hs *http_server) latestConfigState(w http.ResponseWriter) {
+	confJSON := orderedmap.New()
+	schemaJSON := orderedmap.New()
 
-	var schema_json = orderedmap.New()
-	json.Unmarshal([]byte(*(hs.manager.source.getSchema())), &schema_json)
+	json.Unmarshal([]byte(*(hs.manager.source.getConfig())), &confJSON)
+	json.Unmarshal([]byte(*(hs.manager.source.getSchema())), &schemaJSON)
 
-	var modifiable_paths_map = orderedmap.New()
-	modifiable_paths_map.Set("insertable", hs.manager.getInsertablePaths())
-	modifiable_paths_map.Set("removable", hs.manager.getRemovablePaths())
-	modifiable_paths_map.Set("replaceable", hs.manager.getReplaceablePaths())
+	modPaths := orderedmap.New()
+	modPaths.Set("insertable", hs.manager.getInsertablePaths())
+	modPaths.Set("removable", hs.manager.getRemovablePaths())
+	modPaths.Set("replaceable", hs.manager.getReplaceablePaths())
 
-	var body_map = orderedmap.New()
-	body_map.Set("modifiable_paths", modifiable_paths_map)
-	body_map.Set("config", conf_json)
-	body_map.Set("schema", schema_json)
-	body_map.Set("config_hash", HashSHA256(*(hs.manager.source.getConfig())))
+	data := orderedmap.New()
+	data.Set("modifiable_paths", modPaths)
+	data.Set("config", confJSON)
+	data.Set("schema", schemaJSON)
+	data.Set("config_hash", HashSHA256(*(hs.manager.source.getConfig())))
 
-	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Content-Type", "application/json")
-	p, _ := json.MarshalIndent(body_map, "", "  ")
-	io.WriteString(w, string(p))
+	writeSuccess(w, data)
 }
 
-func (hs *http_server) unauthorized_access_resp(w http.ResponseWriter) {
-	w.Header().Set("access_control_allow_origin", "*")
-	w.Header().Set("content_type", "text/html")
-	w.WriteHeader(http.StatusBadRequest)
-	io.WriteString(w, "Unauthorized access")
+/* ------------------ Unified Response Helpers ------------------ */
 
-	// router::res_t res;
-	// res.result(beast::http::status::unauthorized);
-	// res.set(beast::http::field::access_control_allow_origin, "*");
-	// res.set(beast::http::field::content_type, "text/html");
-	// res.body() = "Unauthorized access";
-	// res.prepare_payload();
-	// return res;
+func writeError(w http.ResponseWriter, code int, msg string) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(code)
+
+	errObj := orderedmap.New()
+	errObj.Set("message", msg)
+	errObj.Set("code", code)
+
+	resp := orderedmap.New()
+	resp.Set("success", false)
+	resp.Set("error", errObj)
+
+	out, _ := json.MarshalIndent(resp, "", "  ")
+	w.Write(out)
+}
+
+func writeSuccess(w http.ResponseWriter, data *orderedmap.OrderedMap) {
+	w.Header().Set("Access-Control-Allow-Origin", "*")
+	w.Header().Set("Content-Type", "application/json")
+
+	resp := orderedmap.New()
+	resp.Set("success", true)
+	resp.Set("data", data)
+
+	out, _ := json.MarshalIndent(resp, "", "  ")
+	w.Write(out)
+}
+
+/* ------------------ JSON Extract Helpers ------------------ */
+
+func getString(m *orderedmap.OrderedMap, key string) (string, error) {
+	val, present := m.Get(key)
+	if !present {
+		return "", fmt.Errorf("%s is missing", key)
+	}
+
+	s, ok := val.(string)
+	if !ok {
+		return "", fmt.Errorf("%s must be a string", key)
+	}
+
+	return s, nil
+}
+
+func getIndex(m *orderedmap.OrderedMap) (int, error) {
+	val, present := m.Get("index")
+	if !present {
+		return 0, fmt.Errorf("index is missing")
+	}
+
+	f, ok := val.(float64)
+	if !ok {
+		return 0, fmt.Errorf("index must be a number")
+	}
+
+	return int(f), nil
 }
